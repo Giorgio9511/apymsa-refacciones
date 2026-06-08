8d7461c8325a777129c45567db91d6821b3dd571
Jorge <jorge---9511@hotmail.com>
2026-06-07 18:46:54 -0600

    feat: Se aplica Docker

--- Refacciones.API/Program.cs
+++ Refacciones.API/Program.cs
@@ -46,4 +46,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Aplicar migraciones automáticamente al arrancar
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
